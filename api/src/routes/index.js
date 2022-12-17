adc28539870f1c0c1c3ad3c57e2280e54fa315a2
Mateo Carlos <mateocarlos402@hotmail.com>
2022-12-17 02:55:17 -0300

    First Commit

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 // Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+// Ejemplo: const authRouter = require ('./auth.js');
 
 
 const router = Router();
